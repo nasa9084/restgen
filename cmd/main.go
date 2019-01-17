badcef6e694552314b6065695194729fca184664
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-01-17 18:31:58 +0900

    split new command

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 
 	flags "github.com/jessevdk/go-flags"
-	"github.com/pkg/errors"
 )
 
 var (
@@ -56,78 +53,5 @@ func (cmd VersionCommand) Execute([]string) error {
 		return nil
 	}
 	fmt.Println(version)
-	return nil
-}
-
-type NewCommand struct {
-	Directory string `short:"d" long:"directory" default:"." description:"target directory"`
-	Args      struct {
-		ApplicationName string `positional-arg-name:"APPLICATION_NAME"`
-	} `positional-args:"yes"`
-}
-
-var dirs = []string{
-	"api",
-	"internal/pkg/models",
-	"cmd",
-}
-
-func (cmd NewCommand) Execute([]string) error {
-	if opts.Verbose {
-		log.Printf("create directory: %s", cmd.Directory)
-	}
-	if err := os.MkdirAll(cmd.Directory, 0755); err != nil {
-		return err
-	}
-	for _, dir := range dirs {
-		dirPath := filepath.Join(cmd.Directory, dir)
-		if opts.Verbose {
-			log.Printf("create directory: %s", dirPath)
-		}
-		if err := os.MkdirAll(dirPath, 0755); err != nil {
-			return err
-		}
-	}
-	specFile := filepath.Join(cmd.Directory, "api", "spec.yaml")
-	if opts.Verbose {
-		log.Printf("create OpenAPI Spec file: %s\n", specFile)
-	}
-	if _, err := os.Stat(specFile); err == nil {
-		return errors.New("spec.yaml has been existing")
-	}
-	defaultSpec, err := Assets.Open("/assets/default_spec.yaml.tmpl")
-	if err != nil {
-		return errors.Wrap(err, "opening from assets")
-	}
-	defer defaultSpec.Close()
-	b, err := ioutil.ReadAll(defaultSpec)
-	if err != nil {
-		return err
-	}
-	f, err := os.OpenFile(specFile, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	if _, err := fmt.Fprintf(f, string(b), cmd.Args.ApplicationName); err != nil {
-		return err
-	}
-	if opts.Verbose {
-		log.Printf("generated spec.yaml\n%s", string(b))
-	}
-	srvGo, err := Assets.Open("/assets/server_main.go.tmpl")
-	if err != nil {
-		return err
-	}
-	defer srvGo.Close()
-	srvGoFile := filepath.Join(cmd.Directory, "cmd", "server", "main.go")
-	f, err = os.OpenFile(srvGoFile, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	if opts.Verbose {
-		log.Printf("generated %s", srvGoFile)
-	}
 	return nil
 }
